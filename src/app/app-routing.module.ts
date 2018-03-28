7d3dfc3c84b69e9ff4646939e07402238afba0c4
Mark Walle <mwalle@gmail.com>
2018-03-27 20:27:29 -0700

    form routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
 
 const routes: Routes = [
   // { path: '', redirectTo: '/form', pathMatch: 'full' },
   // { path: 'form', component: DashboardComponent },
   { path: '', redirectTo: 'admin', pathMatch: 'full' },
-  { path: 'admin', component: ContactListComponent }
+  { path: 'admin', component: ContactListComponent },
+  { path: 'form', component: ContactDetailsComponent }
 ];
 
 @NgModule({
