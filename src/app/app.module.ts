ba094223054f0fe33e57a1cabb423b987cc244e0
Mark Walle <mwalle@gmail.com>
2018-03-27 20:40:18 -0700

    stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
+// import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { ContactDetailsComponent } from './contacts/contact-details/contact-details.component';
 import { ContactListComponent } from './contacts/contact-list/contact-list.component';
+import { ContactService } from './contacts/contact.service';
+
 import { AppRoutingModule } from './/app-routing.module';
 
 
 @NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    // HttpModule,
+    AppRoutingModule
+  ],
   declarations: [
     AppComponent,
     ContactDetailsComponent,
     ContactListComponent
   ],
-  imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    AppRoutingModule
-  ],
-  providers: [],
+  
+  providers: [ ContactService ],
   bootstrap: [ AppComponent ]
 })
 export class AppModule { }
